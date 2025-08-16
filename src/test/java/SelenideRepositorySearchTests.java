0e5532859ccfb40f5eee12e0fd559f3d65b0c5ce
Maxim <Rezusjkee@gmail.com>
2025-08-16 19:47:50 +0500

    Delete commentaries

--- src/test/java/SelenideRepositorySearchTests.java
+++ src/test/java/SelenideRepositorySearchTests.java
@@ -13,29 +13,23 @@ public class SelenideRepositorySearchTests {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
-        //Configuration.browserVersion = "114";
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = true;
-        Configuration.timeout = 5000; // default 4000
+        Configuration.timeout = 5000;
     }
 
     @Test
     void shouldFindSelenideRepository() {
         // открыть главную страницу
         open("https://github.com/");
-        // ввести в поле поиска selenide и нажать enter
         $("[data-target='qbsearch-input.inputButtonText']").click();//
         $("#query-builder-test").setValue("selenide").pressEnter();
-        // кликнуть на первый репозиторий из списка найденых
         $("[data-testid='results-list']").$("a").click();
-        // проверка: заголовок selenide/selenide
         $("#repository-container-header").shouldHave(text("selenide / selenide"));
-        // перейти в вики
         $("#wiki-tab").click();
         // проверяем наличие и открываем страницу Soft assertions
         $("#wiki-pages-box").$(withText("Show")).click();
         $("a[href$='SoftAssertions']").shouldBe(visible).click();
-        // проверим что мы на нужной странице по заголовку
         $(".gh-header-title").shouldHave(text("SoftAssertions"));
         // найти примеры кода для JUnit5
         $(".repository-content").shouldHave(text("Using JUnit5 extend test class:"));
@@ -53,7 +47,6 @@ public class SelenideRepositorySearchTests {
                           }
                         }
                         """));
-        // Второй пример
         $(".repository-content ").shouldHave(text("Or register extension inside test class:"));
         $(".markdown-body").shouldHave(text("""
                 class Tests {
