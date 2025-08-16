7fe671057735f99cf7ad4b4158e5e9c712c2c4a2
Maxim <Rezusjkee@gmail.com>
2025-08-16 09:33:29 +0500

    first commint

--- src/test/java/SelenideRepositorySearchTests.java
+++ src/test/java/SelenideRepositorySearchTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
